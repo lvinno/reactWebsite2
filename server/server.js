1bc25012cb22f48074306e2c2ad4864c7d7deb8f
lvino <553269233@qq.com>
2019-02-23 22:44:56 +1100

    'study-mongoose'

--- server/server.js
+++ server/server.js
@@ -1,9 +1,44 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+//连接mongodb
+const DB_URL = 'mongodb://127.0.0.1:27017/imooc';
+mongoose.connect(DB_URL);
+mongoose.connection.on('connected',function(){
+    console.log('mongo connect success')
+})
 
+//新建user
+const User = mongoose.model('user',new mongoose.Schema({
+    user:{type:String,require:true},
+    age:{type:Number,require:true}
+}))
+
+/* User.create({
+    user:'xiao',
+    age:14
+},function(err,doc){
+    if(!err){
+        console.log(doc)
+    }else{
+        console.log(err)
+    }
+}
+)  */
+
+/* User.remove({age:12},function(err,doc){
+    console.log(doc);
+})  */
+
+ User.update({user:'xiao'},{age:100},function(err,doc){
+    console.log(doc)
+}) 
 const app = new express();
 
 app.get('/',function(req,res){
-    res.send('<h1>Hello express</h1>');
+    User.find({},function(err,doc){
+        res.json(doc)
+    })
+   // res.send('<h1>Hello express</h1>');
 })
 
 app.get('/data',function(req,res){
